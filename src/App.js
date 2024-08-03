e959f7c232bf2302178d857330e16d30e7c37e9d
Ritessshhhh <riteshgupta0968@gmail.com>
2024-08-04 00:07:50 +0530

    image slider

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
+import img from './images.jsx';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  let [btn,btnfunc]=useState(0)
+  let x=img.map((v,i)=>{
+    if(btn==i){
+      return(
+        <Img v={v} key={i}/>
+        )
+    }
+    
+  })
+  let next=()=>{
+    if(btn<3){
+      btnfunc(++btn)
+    }
+    else{
+      btnfunc(0)
+    }
+    }
+  let pre=()=>{
+    if(btn>=0){
+    btnfunc(--btn)
+  }
+  else{
+    btnfunc(0)
+  }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="text-center">
+      {x}
+      <button onClick={pre} className={` py-2 px-4 mr-5 bg-blue-300 rounded-lg mt-6 text-[30px] ${btn==0 ? 'hidden':'inline'}`}>previous</button>
+      <button onClick={next} className={`py-2 px-4 bg-blue-300 rounded-lg mt-6 text-[30px] ${btn==3 ? 'hidden' :'inline'}`}>next</button>
     </div>
   );
 }
-
+function Img({v}){
+  return(
+    <>
+      <img src={v.img} className='border-4 border-blue-400 w-[65%] h-[400px] m-auto mt-[85px]'/>
+    </>
+  )
+}
 export default App;
---
+++ src/images.jsx
@@ -0,0 +1,14 @@
+let img=[{
+  img:'/img/item-1.jpeg'
+},
+{
+  img:'/img/item-2.jpeg'
+},
+{
+  img:'/img/item-4.jpeg'
+},
+{
+  img:'/img/item-5.jpeg'
+}]
+
+export default img
